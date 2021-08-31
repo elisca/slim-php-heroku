8e015de043d9a00fb99f917e89697703c1bf6dcf
Ezequiel Lisca <ezequiel.lisca@live.com>
2021-08-31 20:04:52 -0300

    Local XAMPP

--- app/index.php
+++ app/index.php
@@ -12,6 +12,9 @@ require __DIR__ . '/../vendor/autoload.php';
 // Instantiate App
 $app = AppFactory::create();
 
+// Set base path
+$app->setBasePath('/app');
+
 // Add error middleware
 $app->addErrorMiddleware(true, true, true);
 
